0beca7725058f5622b398a0b024b293484745545
mel-wahm <muhammadelqassim03@gmail.com>
2026-05-22 19:41:09 +0100

    psps

--- src/main.py
+++ src/main.py
@@ -1,12 +1,42 @@
 from parser import MapParser
 from models import Connection, Zone
 from graph import Graph
-import pprint
+from visualizer import Renderer
+import os
+import arcade
 
-parser = MapParser("maps/easy/03_basic_capacity.txt")
+maps = [
+    'maps/easy/01_linear_path.txt', #0 
+    'maps/easy/02_simple_fork.txt', #1
+    'maps/easy/03_basic_capacity.txt', #2 
+    'maps/medium/01_dead_end_trap.txt', #3
+    'maps/medium/02_circular_loop.txt', #4
+    'maps/medium/03_priority_puzzle.txt', #5
+    'maps/hard/01_maze_nightmare.txt', #6 
+    'maps/hard/02_capacity_hell.txt', #7
+    'maps/hard/03_ultimate_challenge.txt', #8
+    'maps/challenger/01_the_impossible_dream.txt' #9
+    ]
+parser = MapParser(maps[1])
 parser.parse_map()
 
-graph = Graph(parser.zones, parser.connections)
-graph.adjacency_list()
-for gr, d in graph.graph.items():
-    print(gr, "---->", d)
+# graph = Graph(parser.zones, parser.connections)
+# graph.adjacency_list()
+# for gr, d in graph.graph.items():
+#     print(gr, "---->", d)
+
+red_color = '\033[91m'
+green_color = '\033[92m'
+end_color = '\033[0m'
+bald_access = '\033[1m'
+try:
+    r = Renderer(parser, parser.connections)
+    arcade.run()
+except KeyboardInterrupt:
+    os.system("clear")
+    print(f"{bald_access}{green_color}--------------------------------------{end_color}")
+    print(f"{bald_access}{red_color} The program was stopped by the user{end_color}")
+    print(f"{bald_access}{green_color}--------------------------------------{end_color}")
+    print()
+except Exception as e:
+    print(f"{bald_access}{red_color}Error:", e)
--- src/visualizer.py
+++ src/visualizer.py
@@ -1,13 +1,27 @@
 import arcade
-from parser import MapParser
 
 class Renderer(arcade.Window):
-    def __init__(self):
-        super().__init__(1280,  720,  "Charlie Kirk.")
-    
+    def __init__(self, parser, connections):
+        super().__init__(1280, 720, parser.map_path)
+        self.parser = parser
+        self.connections = connections
+        cors = [cor.coordinates for cor in self.parser.zones.values()]
+        self.avg_x = sum(cor[0] for cor in cors) / len(cors)
+        self.avg_y = sum(cor[1] for cor in cors) / len(cors)
+
     def on_draw(self):
         self.clear()
-
-
-t = Renderer()
-arcade.run()
+        cx = self.width / 2
+        cy = self.height / 2
+        for con in self.connections:
+            
+        for name, zone in self.parser.zones.items():
+            x, y = zone.coordinates
+            color = getattr(arcade.color, zone.color.upper())
+            arcade.draw_circle_filled(cx + (x - self.avg_x) * 150,
+                                      cy + (y - self.avg_y) * 150,
+                                      15,
+                                      color)
+            arcade.draw_text(name, cx + (x - self.avg_x) * 150,
+                             cy + 30 + (y - self.avg_y) * 150,
+                             arcade.color.WHITE, 12, anchor_x="center")
